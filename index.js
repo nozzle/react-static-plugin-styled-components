901ce5a04fc1eb5c1b7d44b2eb1f04fe123bb8b6
Tanner Linsley <tannerlinsley@gmail.com>
2018-07-13 09:27:15 -0600

    Use Babel to compile lib

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const React = require("react");
-const { ServerStyleSheet } = require("styled-components");
+import { ServerStyleSheet } from "styled-components";
 
-module.exports = {
-  // Use beforeRenderToHtml to extract the styles into the page meta
-  beforeRenderToHtml: (app, { meta }) => {
-    meta.styleComponentsSheet = new ServerStyleSheet();
-    return meta.styleComponentsSheet.collectStyles(app);
-  },
-  Head: ({ meta }) => (
-    // The styles aren't fully extraced until the react component
-    // has been rendered to an html string. So we wait until the head
-    // to call the sheet's getStyleElement function
-    // and then insert the tag into the Head
-    <React.Fragment>
-      {meta.styleComponentsSheet.getStyleElement()}
-    </React.Fragment>
-  )
+// Use beforeRenderToHtml to extract the styles into the page meta
+export const beforeRenderToHtml = (app, { meta }) => {
+  meta.styleComponentsSheet = new ServerStyleSheet();
+  return meta.styleComponentsSheet.collectStyles(app);
 };
+
+export const Head = ({ meta }) => (
+  // The styles aren't fully extraced until the react component
+  // has been rendered to an html string. So we wait until the head
+  // to call the sheet's getStyleElement function
+  // and then insert the tag into the Head
+  meta.styleComponentsSheet.getStyleElement()
+);
