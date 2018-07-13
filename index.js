c18e2662a46fb7d776f1e04cf814f46aeaa5873b
Tanner Linsley <tannerlinsley@gmail.com>
2018-07-13 12:23:41 -0600

    Use beforeRenderToComponent instead of app

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+import React from "react";
 import { ServerStyleSheet } from "styled-components";
 
 // Use beforeRenderToHtml to extract the styles into the page meta
-export const beforeRenderToHtml = (app, { meta }) => {
+export const beforeRenderToComponent = (app, { meta }) => {
   meta.styleComponentsSheet = new ServerStyleSheet();
-  return meta.styleComponentsSheet.collectStyles(app);
+  return props => (
+    <StyleSheetManager sheet={meta.styleComponentsSheet.instance}>
+      <App {...props} />
+    </StyleSheetManager>
+  );
 };
 
-export const Head = ({ meta }) => (
+export const Head = ({ meta }) =>
   // The styles aren't fully extraced until the react component
   // has been rendered to an html string. So we wait until the head
   // to call the sheet's getStyleElement function
   // and then insert the tag into the Head
-  meta.styleComponentsSheet.getStyleElement()
-);
+  meta.styleComponentsSheet.getStyleElement();
