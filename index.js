2279ae7ce766c76912d6a88a2fd61b111cdc855e
Tanner Linsley <tannerlinsley@gmail.com>
2018-07-16 10:51:21 -0600

    Use new plugin api

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
-// Use beforeRenderToHtml to extract the styles into the page meta
-export const beforeRenderToComponent = (app, { meta }) => {
-  meta.styleComponentsSheet = new ServerStyleSheet();
-  return props => (
-    <StyleSheetManager sheet={meta.styleComponentsSheet.instance}>
-      <App {...props} />
-    </StyleSheetManager>
-  );
-};
-
-export const Head = ({ meta }) =>
-  // The styles aren't fully extraced until the react component
-  // has been rendered to an html string. So we wait until the head
-  // to call the sheet's getStyleElement function
-  // and then insert the tag into the Head
-  meta.styleComponentsSheet.getStyleElement();
+export default options => ({
+  // Use beforeRenderToHtml to extract the styles into the page meta
+  beforeRenderToComponent: (App, { meta }) => {
+    meta.styleComponentsSheet = new ServerStyleSheet();
+    return props => (
+      <StyleSheetManager sheet={meta.styleComponentsSheet.instance}>
+        <App {...props} />
+      </StyleSheetManager>
+    );
+  },
+  Head: ({ meta }) =>
+    // The styles aren't fully extraced until the react component
+    // has been rendered to an html string. So we wait until the head
+    // to call the sheet's getStyleElement function
+    // and then insert the tag into the Head
+    meta.styleComponentsSheet.getStyleElement()
+});
